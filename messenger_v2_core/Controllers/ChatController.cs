02683f2a020b24bb4db3b24a9690c5ebfe7c1300
oksymar <mkmmiichchaall@gmail.com>
2018-05-29 15:24:19 +0200

    (WIP) MQTT broken

--- messenger_v2_core/Controllers/ChatController.cs
+++ messenger_v2_core/Controllers/ChatController.cs
@@ -10,33 +10,33 @@ namespace messenger_v2.Controllers
 {
     public class ChatController : Controller
     {
-        private const String MQTT_BROKER_ADDR = "192.168.0.248";
+        private const String MQTT_BROKER_ADDR = "192.168.0.199";
 
-        private MqttClient client;
+//        private MqttClient client;
 
-        private class MsgObject
-        {
-            private String message;
-            private long timestamp;
-
-            public MsgObject(string message, long timestamp)
-            {
-                this.message = message;
-                this.timestamp = timestamp;
-            }
-
-            public string Message
-            {
-                get => message;
-                set => message = value;
-            }
-
-            public long Timestamp
-            {
-                get => timestamp;
-                set => timestamp = value;
-            }
-        }
+//        private class MsgObject
+//        {
+//            private String message;
+//            private long timestamp;
+//
+//            public MsgObject(string message, long timestamp)
+//            {
+//                this.message = message;
+//                this.timestamp = timestamp;
+//            }
+//
+//            public string Message
+//            {
+//                get => message;
+//                set => message = value;
+//            }
+//
+//            public long Timestamp
+//            {
+//                get => timestamp;
+//                set => timestamp = value;
+//            }
+//        }
 
         private static Queue globalMsgStorage = new Queue();
 
@@ -45,58 +45,58 @@ namespace messenger_v2.Controllers
         {
             ViewBag.Title = "Chat Page";
 
-            if (client == null)
-            {
-                client = new MqttClient(MQTT_BROKER_ADDR);
-            }
-
-            client.MqttMsgPublishReceived += ReceiveMessageMQTT;
-            string clientId = Guid.NewGuid().ToString();
-            client.Connect(clientId);
-            client.Subscribe(new string[] {"/chat"}, new byte[] {MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE});
+//            if (client == null)
+//            {
+//                client = new MqttClient(MQTT_BROKER_ADDR);
+//            }
+//
+//            client.MqttMsgPublishReceived += ReceiveMessageMQTT;
+//            string clientId = Guid.NewGuid().ToString();
+//            client.Connect(clientId);
+//            client.Subscribe(new string[] {"/chat"}, new byte[] {MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE});
 
             return View();
         }
 
-        [HttpPost]
-        public void SendMessageMQTT(string message)
-        {
-            if (client == null)
-            {
-                client = new MqttClient(MQTT_BROKER_ADDR);
-                string clientId = Guid.NewGuid().ToString();
-                client.Connect(clientId);
-            }
+//        [HttpPost]
+//        public void SendMessageMQTT(string message)
+//        {
+//            if (client == null)
+//            {
+//                client = new MqttClient(MQTT_BROKER_ADDR);
+//                string clientId = Guid.NewGuid().ToString();
+//                client.Connect(clientId);
+//            }
+//
+//            client.Publish("/chat", Encoding.UTF8.GetBytes(message), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE,
+//                false);
+//        }
 
-            client.Publish("/chat", Encoding.UTF8.GetBytes(message), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE,
-                false);
-        }
+//        public void ReceiveMessageMQTT(object sender, MqttMsgPublishEventArgs e)
+//        {
+//            var entity = JsonConvert.DeserializeObject<MsgObject>(Encoding.UTF8.GetString(e.Message));
+//            globalMsgStorage.Enqueue(entity);
+//        }
 
-        public void ReceiveMessageMQTT(object sender, MqttMsgPublishEventArgs e)
-        {
-            var entity = JsonConvert.DeserializeObject<MsgObject>(Encoding.UTF8.GetString(e.Message));
-            globalMsgStorage.Enqueue(entity);
-        }
-
-        [HttpGet]
-        public string ReceiveMessage()
-        {
-            if (globalMsgStorage.Count != 0)
-            {
-                MsgObject element = (MsgObject) globalMsgStorage.Peek();
-                if (element.Timestamp + 600 < DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
-                {
-                    MsgObject msgObj = (MsgObject) globalMsgStorage.Dequeue();
-                    return JsonConvert.SerializeObject(msgObj);
-                }
-                else
-                {
-                    MsgObject msgObj = (MsgObject) globalMsgStorage.Peek();
-                    return JsonConvert.SerializeObject(msgObj);
-                }
-            }
-
-            return "Error";
-        }
+//        [HttpGet]
+//        public string ReceiveMessage()
+//        {
+//            if (globalMsgStorage.Count != 0)
+//            {
+//                MsgObject element = (MsgObject) globalMsgStorage.Peek();
+//                if (element.Timestamp + 600 < DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
+//                {
+//                    MsgObject msgObj = (MsgObject) globalMsgStorage.Dequeue();
+//                    return JsonConvert.SerializeObject(msgObj);
+//                }
+//                else
+//                {
+//                    MsgObject msgObj = (MsgObject) globalMsgStorage.Peek();
+//                    return JsonConvert.SerializeObject(msgObj);
+//                }
+//            }
+//
+//            return "Error";
+//        }
     }
 }
--- messenger_v2_core/wwwroot/js/chatPage.js
+++ messenger_v2_core/wwwroot/js/chatPage.js
@@ -1,6 +1,8 @@
-var timestamp;
+// var client;
+var MQTT_BROKER_ADDR = "192.168.0.199";
 
-seTimer();
+//seTimer();
+connectMQTT();
 
 function sendMessage() {
     var messageWindow = document.getElementsByName("messageWindow")[0];
@@ -11,26 +13,60 @@ function sendMessage() {
 
     messageWindow.value = "";
 
-    $.ajax({
-        url: '/Chat/SendMessageMQTT',
-        type: 'POST',
-        data: {message: JSON.stringify(messageMQTT)}
+    var messagePaho = new Paho.MQTT.Message(JSON.stringify(messageMQTT));
+    messagePaho.destinationName = "/chat";
+    client.send(messagePaho);
+
+    // $.ajax({
+    //     url: '/Chat/SendMessageMQTT',
+    //     type: 'POST',
+    //     data: {message: JSON.stringify(messageMQTT)}
+    // });
+}
+
+// function seTimer() {
+//     setInterval(function () {
+//         $.ajax({
+//             url: '/Chat/ReceiveMessage',
+//             type: 'GET',
+//             success: handlereceivedMessage
+//         });
+//     }, 500);
+//
+// }
+
+// function handlereceivedMessage(data) {
+//     if (data !== "Error") {
+//         $("#ChatBox").append("<span>"+data.message+"</span>");
+//     }
+// }
+
+//PAHO
+function connectMQTT() {
+    //client = new Paho.MQTT.Client(MQTT_BROKER_ADDR, Number(1883),"", "clientId");
+    client = new Paho.MQTT.Client("test.mosquitto.org", Number(443), "clientId");
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+    client.connect({
+        onSuccess: onConnect
     });
 }
 
-function seTimer() {
-    setInterval(function () {
-        $.ajax({
-            url: '/Chat/ReceiveMessage',
-            type: 'GET',
-            success: handlereceivedMessage
-        });
-    }, 500);
-
+// called when the client connects
+function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+    client.subscribe("/chat");
 }
 
-function handlereceivedMessage(data) {
-    if (data !== "Error") {
-        $("#ChatBox").append("<span>"+data.message+"</span>");
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:" + responseObject.errorMessage);
     }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+    $("#ChatBox").append("<span>" + data.message + "</span>");
 }
